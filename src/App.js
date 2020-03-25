cc4b8aa533cf2351fa3f032a1f1c7f65fc93a797
Senatova93 <nanniin160@gmail.com>
2020-03-25 23:57:12 +0300

    task3

--- src/App.js
+++ src/App.js
@@ -2,25 +2,39 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends React.Component {
+    input_ref = React.createRef()
+    state = {
+        add: [],
+        num:0
+    }
+
+    clickButton = () => {
+        let newNum = this.input_ref.current.value;
+        let num=this.state.num;
+        if (newNum) {
+            this.input_ref.current.value = ''
+            num++
+            let newNum1={num:newNum}
+            let newNums=[...this.state.add,newNum1]
+            this.setState({
+                add:newNums,
+                num:num
+            })
+            alert(`Вы ввеели: ${newNum} `)
+        }
+    }
+
+    render = () => {
+        return (
+            <div className="App">
+
+                <input  ref={this.input_ref} type="text" placeholder="Введите ваш запрос:"/>
+                <span> Число нажатий: {this.state.num} </span>
+                <button onClick={this.clickButton} > Нажать </button>
+                        </div> )
+
+
+}}
 
 export default App;
